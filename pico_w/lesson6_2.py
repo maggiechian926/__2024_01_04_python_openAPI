6f578e18e7d46530b5e8ca008454dc773f1c9a70
maggiechiang <maggiechiang.offcial@gmail.com>
2024-01-12 15:41:00 +0800

    modify

--- pico_w/lesson6_2.py
+++ pico_w/lesson6_2.py
@@ -1,16 +1,34 @@
 from machine import Pin
 import time
 
-start_ticks = time.ticks_ms()
+start_ticks1 = time.ticks_ms()
+start_ticks2 = time.ticks_ms()
+start_ticks3 = time.ticks_ms()
 led25 = Pin ("LED",Pin.OUT)
 ledStatus = False
 
 while True:
-   var_ticks = time.ticks_ms()
-   if var_ticks - start_ticks >= 1000:
-       start_ticks = time.ticks_ms()
-       ledStatus = not ledStatus 
-       led25.value(ledStatus)
+    if time.ticks_diff(time.ticks_ms(), start_ticks1) >= 1000:
+        print("千年彗星降臨過1秒了")
+        start_ticks1 = time.ticks_ms()
+        ledStatus = not ledStatus
+        led25.value(ledStatus)
+       
+       
+    if time.ticks_diff(time.ticks_ms(), start_ticks2) >= 5000:
+        print("恐龍時代過5秒了")
+        start_ticks2 = time.ticks_ms()
+        ledStatus = not ledStatus
+        led25.value(ledStatus)
+        
+    if time.ticks_diff(time.ticks_ms(), start_ticks3) >= 10000:
+        print("原始時代過10秒了")
+        start_ticks3 = time.ticks_ms()
+        ledStatus = not ledStatus
+        led25.value(ledStatus)
+    
+       
+       
        
    
     
